0edd3b13fe7bcb4cd539fa6651b28f9472a1f8fa
Kasper Laudrup <laudrup@stacktrace.dk>
2018-06-27 21:55:03 +0200

    Default to production when running under wsgi
    Wsgi will be used under production, so default to Production settings
    when running under wsgi.

--- ebbamåla/wsgi.py
+++ ebbamåla/wsgi.py
@@ -4,7 +4,7 @@ WSGI config for ebbamåla project.
 import os
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ebbamåla.settings')
-os.environ.setdefault('DJANGO_CONFIGURATION', 'Development')
+os.environ.setdefault('DJANGO_CONFIGURATION', 'Production')
 
 from configurations.wsgi import get_wsgi_application
 
