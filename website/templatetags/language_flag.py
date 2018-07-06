16a6aa0fdf7e04d6968c85e15984b8139d09a9c1
Kasper Laudrup <laudrup@stacktrace.dk>
2018-07-06 10:47:09 +0200

    Added missing template tag
    Forgot to add the custom language_flag template tag

---
+++ website/templatetags/language_flag.py
@@ -0,0 +1,18 @@
+from django.utils.translation import get_language
+from django import template
+from django.utils.safestring import mark_safe
+
+register = template.Library()
+
+LANG_TO_FLAG = {
+    'da': 'flag-icon-dk',
+    'en': 'flag-icon-gb',
+}
+
+
+@register.simple_tag
+def language_flag(lang=None):
+    if not lang:
+        lang = get_language()
+    lang_flag = LANG_TO_FLAG[lang]
+    return mark_safe('<span class="flag-icon {}"></span>'.format(lang_flag))
