19de9a49290c51820d99639de1a17d38dec81bf0
Kasper Laudrup <laudrup@stacktrace.dk>
2018-07-25 22:09:09 +0200

    Don't raise exception if no header images are uploaded
    If a no header images are added to the HeaderImage model, simply
    return an empty string instead of raising an exception.
    
    This makes (automatic) testing a lot easier.

--- website/templatetags/header_image.py
+++ website/templatetags/header_image.py
@@ -8,4 +8,7 @@ register = template.Library()
 @register.simple_tag
 def header_image():
     header_images = HeaderImage.objects.order_by('?')
-    return header_images[0].photo.url
+    if len(header_images) > 0:
+        return header_images[0].photo.url
+    else:
+        return ''
