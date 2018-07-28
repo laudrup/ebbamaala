6adc2784da7192150a340d4d63519c30f4bc2bad
Kasper Laudrup <laudrup@stacktrace.dk>
2018-07-29 01:41:13 +0200

    Test saving photos with timestamp
    Test that the timestamps are read from the EXIF metadata and used when
    saving the photo in the gallery

--- website/tests/test_gallery.py
+++ website/tests/test_gallery.py
@@ -1,5 +1,7 @@
 from io import BytesIO
 from unittest import mock
+import piexif
+from datetime import datetime
 
 from django.core.files.uploadedfile import SimpleUploadedFile
 from django.test import TestCase
@@ -24,17 +26,41 @@ class GalleryPhotoModelTests(TestCase):
         self.flock_patcher.stop()
 
     def test_photo_upload(self):
+        # Create image
         img = Image.new('RGB', (10, 10))
         byte_io = BytesIO()
         img.save(byte_io, 'JPEG')
 
+        # Save to gallery
         gallery = Gallery.objects.create(title='Test Gallery')
         GalleryPhoto.objects.create(photo=SimpleUploadedFile('test.jpg', byte_io.getvalue()),
                                     gallery=gallery)
 
+        # There should be one photo
         gallery_photos = gallery.galleryphoto_set.all()
         self.assertEqual(1, len(gallery_photos))
 
+        # Which is the one just uploaded
         photo = gallery_photos[0]
         self.assertEqual('test.jpg', str(photo))
         self.assertIsNone(photo.date)
+
+    def test_photo_timestamp(self):
+        # Create EXIF timestamp
+        timestamp = datetime(2015, 8, 15)
+        exif_dict = {'Exif': {piexif.ExifIFD.DateTimeOriginal:
+                              timestamp.strftime('%Y:%m:%d %H:%M:%S')}}
+        exif_bytes = piexif.dump(exif_dict)
+
+        # Create image with EXIF timestamp
+        byte_io = BytesIO()
+        img = Image.new('RGB', (10, 10))
+        img.save(byte_io, 'JPEG', exif=exif_bytes)
+
+        # Save image to gallery
+        gallery = Gallery.objects.create(title='Test Gallery')
+        photo = GalleryPhoto.objects.create(photo=SimpleUploadedFile('test.jpg',
+                                                                     byte_io.getvalue()),
+                                            gallery=gallery)
+
+        self.assertEqual(timestamp, photo.date)
