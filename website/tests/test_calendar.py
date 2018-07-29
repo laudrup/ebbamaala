dae56d307224742d3b0855f672806df15a87309a
Kasper Laudrup <laudrup@stacktrace.dk>
2018-07-29 19:42:19 +0200

    Test that holidays are displayed in the HTML calendar
    Test that a holiday will be displayed in the calendar when returned
    from the holidays API.

--- website/tests/test_calendar.py
+++ website/tests/test_calendar.py
@@ -1,3 +1,8 @@
+import datetime
+from unittest import mock
+
+from bs4 import BeautifulSoup
+from django.conf import settings
 from django.template import Context, Template
 from django.test import TestCase
 from django.utils import translation
@@ -48,3 +53,41 @@ class MonthNameTagTests(TestCase):
                 '{{ month|month_name }}'
             )
             self.assertEqual(name, template_to_render.render(context))
+
+
+class CalendarTagTests(TestCase):
+
+    @mock.patch('salling_group_holidays.v1')
+    def test_holiday_api_key(self, mock_holidays_api):
+        self._render_template()
+        mock_holidays_api.assert_called_with(settings.DS_API_KEY)
+
+    @mock.patch('salling_group_holidays.v1.holidays')
+    def test_holidays(self, mock_holidays):
+        mock_holidays.return_value = {datetime.date(2018, 1, 17):
+                                      {'name': 'Robanukah',
+                                       'holiday': True}}
+
+        soup = BeautifulSoup(self._render_template(), 'lxml')
+        mock_holidays.assert_called_with(datetime.date(2018, 1, 1),
+                                         datetime.date(2018, 1, 31))
+
+        days = soup.findAll('div', {'class': ['current-month']})
+        self.assertEqual(31, len(days))
+
+        holiday = days[16].find('div', {'class': 'd-xl-block'})
+        self.assertIsNotNone(holiday)
+        self.assertEqual(1, len(holiday.contents))
+        self.assertHTMLEqual('Robanukah', holiday.contents[0])
+
+    def _render_template(self):
+        context = Context(
+            {'year': 2018,
+             'month': 1,
+             'bookings': []}
+        )
+        template_to_render = Template(
+            '{% load calendar %}'
+            '{% calendar year month bookings %}'
+        )
+        return template_to_render.render(context)
