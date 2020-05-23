3694c7aa33c63cd30807148fd6c718c2d2f59e23
Kasper Laudrup <laudrup@stacktrace.dk>
2020-05-23 23:58:19 +0200

    Add custom error pages
    Add some more personal error pages with a sad moose and add some urls
    for testing them during development

--- ebbamåla/urls.py
+++ ebbamåla/urls.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('', include('website.urls')),
@@ -12,3 +13,5 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += [path('404/', TemplateView.as_view(template_name='404.html'), name='404-test'),
+                    path('500/', TemplateView.as_view(template_name='500.html'), name='500-test')]
