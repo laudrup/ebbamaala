d39576f5b5a4e570b259bcdd1e6bc8c922a55f1c
Kasper Laudrup <laudrup@stacktrace.dk>
2023-04-04 21:20:14 +0200

    Add tests for gallery view
    Extend the tests to cover the gallery views to reduce the risk of
    breaking things during refactoring

--- website/tests/test_gallery.py
+++ website/tests/test_gallery.py
@@ -5,9 +5,11 @@ from unittest import mock
 import piexif
 import os.path
 import tzdata
+from django.contrib.auth import get_user_model
 from django.core.files.uploadedfile import SimpleUploadedFile
 from django.test import TestCase
 from django.utils import timezone
+from bs4 import BeautifulSoup
 from PIL import Image
 from pyfakefs.fake_filesystem_unittest import Patcher
 from website.models import Gallery, GalleryPhoto
@@ -18,8 +20,17 @@ utc_tz_data = b'''TZif2\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\
 \x00\x00\x00\x04\x00\x00\x00\x00\x00\x00UTC\x00\nUTC0\n'''
 
 
-class GalleryPhotoModelTests(TestCase):
+def create_jpeg_data(exif={}):
+    byte_io = BytesIO()
+    img = Image.new('RGB', (10, 10))
+    if exif:
+        img.save(byte_io, 'JPEG', exif=piexif.dump(exif))
+    else:
+        img.save(byte_io, 'JPEG')
+    return byte_io.getvalue()
 
+
+class GalleryPhotoModelTests(TestCase):
     def setUp(self):
         # Use pyfakefs for filesystem access
         self.fs_patcher = Patcher()
@@ -35,7 +46,7 @@ class GalleryPhotoModelTests(TestCase):
         self.flock_patcher.stop()
 
     def test_photo_upload(self):
-        jpeg_data = self._create_jpeg_data()
+        jpeg_data = create_jpeg_data()
         gallery = Gallery.objects.create(title='Test Gallery')
         GalleryPhoto.objects.create(photo=SimpleUploadedFile('test.jpg', jpeg_data),
                                     gallery=gallery)
@@ -43,7 +54,6 @@ class GalleryPhotoModelTests(TestCase):
         # There should be one photo
         gallery_photos = gallery.galleryphoto_set.all()
         self.assertEqual(1, len(gallery_photos))
-
         # Which is the one just uploaded
         photo = gallery_photos[0]
         self.assertEqual('test.jpg', str(photo))
@@ -54,7 +64,7 @@ class GalleryPhotoModelTests(TestCase):
         timestamp = timezone.make_aware(datetime(2015, 8, 15))
         exif = {'Exif': {piexif.ExifIFD.DateTimeOriginal:
                          timestamp.strftime('%Y:%m:%d %H:%M:%S')}}
-        jpeg_data = self._create_jpeg_data(exif=exif)
+        jpeg_data = create_jpeg_data(exif=exif)
 
         # Save image to gallery
         gallery = Gallery.objects.create(title='Test Gallery')
@@ -65,8 +75,8 @@ class GalleryPhotoModelTests(TestCase):
     @mock.patch('website.models.logger')
     def test_invalid_orientation_tag(self, mock_logger):
         gallery = Gallery.objects.create(title='Test Gallery')
-        jpeg_data = self._create_jpeg_data(exif={'0th': {piexif.ImageIFD.Orientation:
-                                                         666}})
+        jpeg_data = create_jpeg_data(exif={'0th': {piexif.ImageIFD.Orientation:
+                                                   666}})
 
         GalleryPhoto.objects.create(photo=SimpleUploadedFile('test.jpg', jpeg_data),
                                     gallery=gallery)
@@ -80,8 +90,8 @@ class GalleryPhotoModelTests(TestCase):
 
         for rotation in range(1, 9):
             # Create image with EXIF rotation info
-            jpeg_data = self._create_jpeg_data(exif={'0th': {piexif.ImageIFD.Orientation:
-                                                             rotation}})
+            jpeg_data = create_jpeg_data(exif={'0th': {piexif.ImageIFD.Orientation:
+                                                       rotation}})
 
             # Save image to gallery
             photo = GalleryPhoto.objects.create(photo=SimpleUploadedFile('test.jpg', jpeg_data),
@@ -95,11 +105,27 @@ class GalleryPhotoModelTests(TestCase):
             self.assertIn(piexif.ImageIFD.Orientation, exif_dict['0th'])
             self.assertEqual(exif_dict['0th'][piexif.ImageIFD.Orientation], 1)
 
-    def _create_jpeg_data(self, exif={}):
-        byte_io = BytesIO()
-        img = Image.new('RGB', (10, 10))
-        if exif:
-            img.save(byte_io, 'JPEG', exif=piexif.dump(exif))
-        else:
-            img.save(byte_io, 'JPEG')
-        return byte_io.getvalue()
+
+class GalleryPhotoViewTests(TestCase):
+    def setUp(self):
+        get_user_model().objects.create_user('bobby', 'littlebobby@gmail.com', 'tables')
+        self.client.login(username='bobby', password='tables')
+
+    def test_no_galleries(self):
+        response = self.client.get('/gallery')
+        self.assertEqual(200, response.status_code)
+
+    def test_empty_gallery(self):
+        gallery = Gallery.objects.create(title='Test Gallery')
+        response = self.client.get(gallery.get_absolute_url())
+        self.assertEqual(200, response.status_code)
+
+    def test_gallery_photo(self):
+        gallery = Gallery.objects.create(title='Test Gallery')
+        photo = GalleryPhoto.objects.create(photo=SimpleUploadedFile('test.jpg', create_jpeg_data()),
+                                            gallery=gallery)
+        response = self.client.get(gallery.get_absolute_url())
+        self.assertEqual(200, response.status_code)
+        soup = BeautifulSoup(response.content, 'lxml')
+        self.assertIn(photo.get_absolute_url(), [a['href'] for a in soup.find_all('a', href=True)])
+        self.assertEqual(200, self.client.get(photo.get_absolute_url()).status_code)
