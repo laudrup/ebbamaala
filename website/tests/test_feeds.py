7f23c739e8f98ed2a48a849b5142b2674d965cbc
Kasper Laudrup <laudrup@stacktrace.dk>
2023-04-09 20:35:13 +0200

    Update ics to 0.8.0-dev0
    Upgrade ics library used for testing the booking feed to latest
    development version. This is mainly to workaround this issue:
    
    https://github.com/ics-py/ics-py/issues/373
    
    but also means a few more members a available in the parsed event that
    can be used for testing.

--- website/tests/test_feeds.py
+++ website/tests/test_feeds.py
@@ -3,7 +3,7 @@ from datetime import date
 
 from django.contrib.auth import get_user_model
 from django.test import TestCase
-from ics import Calendar
+from ics import Calendar, geo
 
 from website.models import Booking
 
@@ -12,39 +12,39 @@ class calendarFeedTests(TestCase):
     def setUp(self):
         self.user = get_user_model().objects.create_user('bobby', 'littlebobby@gmail.com', 'tables')
 
-    def get_calendar(self):
-        b64auth = base64.b64encode(b'bobby:tables').decode('utf-8')
-        response = self.client.get('/bookings.ics', {}, HTTP_AUTHORIZATION=f'Basic {b64auth}')
-        self.assertEqual(200, response.status_code)
-        return Calendar(response.content.decode('utf-8'))
-
     def test_not_authenticated(self):
         response = self.client.get('/bookings.ics', {}, HTTP_ACCEPT='text/calendar')
         self.assertEqual(401, response.status_code)
 
     def test_empty_calendar(self):
-        calendar = self.get_calendar()
+        b64auth = base64.b64encode(b'bobby:tables').decode('utf-8')
+        response = self.client.get('/bookings.ics', {}, HTTP_AUTHORIZATION=f'Basic {b64auth}')
+        self.assertEqual(200, response.status_code)
+        calendar = Calendar(response.content.decode('utf-8'))
         self.assertFalse(calendar.events)
 
     def test_calendar_entry(self):
-        calendar = self.get_calendar()
-        self.assertFalse(calendar.events)
-
         start_date = date(2023, 7, 20)
         end_date = date(2023, 7, 26)
         booker = 'Bamse'
         description = 'Hyggetur med kylling'
-        Booking(start_date=start_date,
-                end_date=end_date,
-                user=self.user,
-                booker=booker,
-                description=description).save()
+        booking = Booking.objects.create(start_date=start_date,
+                                         end_date=end_date,
+                                         user=self.user,
+                                         booker=booker,
+                                         description=description)
         self.assertEqual(1, len(Booking.objects.all()))
 
-        calendar = self.get_calendar()
+        b64auth = base64.b64encode(b'bobby:tables').decode('utf-8')
+        response = self.client.get('/bookings.ics', {}, HTTP_AUTHORIZATION=f'Basic {b64auth}')
+        self.assertEqual(200, response.status_code)
+        calendar = Calendar(response.content.decode('utf-8'))
+
         self.assertEqual(1, len(calendar.events))
         event = calendar.events.pop()
-        self.assertEquals(f'{booker}s booking', event.name)
-        self.assertEqual(start_date, event.begin.datetime.date())
-        self.assertEqual(end_date, event.end.datetime.date())
+        self.assertEquals(f'{booker}s booking', event.summary)
+        self.assertEqual(start_date, event.begin.date())
+        self.assertEqual(end_date, event.end.date())
         self.assertEqual(event.description, description)
+        self.assertEquals(response.wsgi_request.build_absolute_uri(booking.get_absolute_url()), event.url)
+        self.assertEqual(geo.Geo(latitude=56.500111, longitude=15.471333), event.geo)
