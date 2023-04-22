0ab4c9796ce48ea6e5721592ebf7f748c2650e9c
Kasper Laudrup <laudrup@stacktrace.dk>
2023-04-22 22:51:37 +0200

    Use file time stamp to get info page revisions
    Running git as another user than the one who checked out the
    repository causes git to fail for security reasons.
    
    Since this is currently how the code is being deployed and there's no
    easy way to work around that just use the timestamp of the file
    instead.

--- website/templatetags/template_revision.py
+++ website/templatetags/template_revision.py
@@ -1,4 +1,4 @@
-import subprocess
+import os
 from datetime import datetime
 
 from django import template
@@ -9,5 +9,5 @@ register = template.Library()
 @register.filter()
 def template_revision(name):
     file_path = str(template.loader.get_template(name).template.origin)
-    file_revision = subprocess.check_output(['git', 'log', '-1', '--pretty=%cI', file_path])
-    return datetime.fromisoformat(file_revision.decode('utf-8').strip())
+    mtime = os.path.getmtime(file_path)
+    return datetime.fromtimestamp(mtime)
