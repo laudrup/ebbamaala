2689b119c97cb60fcf3b2f688ef3571e7c30c5eb
Kasper Laudrup <laudrup@stacktrace.dk>
2023-04-22 23:01:47 +0200

    Remove unused url paths for testing error views

--- ebbamåla/urls.py
+++ ebbamåla/urls.py
@@ -2,7 +2,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
-from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('', include('website.urls')),
@@ -13,5 +12,3 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += [path('404/', TemplateView.as_view(template_name='404.html'), name='404-test'),
-                    path('500/', TemplateView.as_view(template_name='500.html'), name='500-test')]
