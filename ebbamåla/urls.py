5a5e4c038e8cb5eb7cb3962d615b3722fdfa1b48
Kasper Laudrup <laudrup@stacktrace.dk>
2023-04-22 23:02:59 +0200

    Move all handling of media paths to website application

--- ebbamåla/urls.py
+++ ebbamåla/urls.py
@@ -1,5 +1,3 @@
-from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
@@ -9,6 +7,3 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     path('i18n/', include('django.conf.urls.i18n')),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
