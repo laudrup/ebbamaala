0022b5adca30f8f6e9ecae34524d681fc33225ac
Kasper Laudrup <laudrup@stacktrace.dk>
2025-04-05 17:22:45 +0200

    Update section on wood sorting
    We have introduced a new system for how to sort wood so update the
    section on that including new graphics that can fit in portrait when
    rendered as PDF.

--- website/views.py
+++ website/views.py
@@ -56,7 +56,7 @@ class InfoView(TemplateView):
         },
         'wood': {
             'title': gettext_noop('Wood'),
-            'orientation': 'landscape',
+            'orientation': 'potrait',
         },
     }
 
